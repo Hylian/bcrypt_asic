9f987efcb44033ecaf1a79fb2809d8e42fd76fdf
Edward Shin <edshin95@gmail.com>
2016-04-11 17:52:04 -0400

    add python test script

---
+++ ref/bcrypt_test.py
@@ -0,0 +1,14 @@
+import bcrypt
+from bcrypt import _bcrypt
+
+key = b"mykey"
+salt = b'\xb9\xe4\x03\x30\xd2\xc1\x0b\xbd\x8b\xd3\x0c\xbd\x02\x20\xce\xea'
+rounds = 10
+
+saltout = _bcrypt.ffi.new("unsigned char[]", 30)
+gensalt = _bcrypt.lib.crypt_gensalt_rn(b"$" + b"2b" + b"$", rounds, salt, len(salt), saltout, len(saltout))
+
+hashed = _bcrypt.ffi.new("unsigned char[]", 128)
+_bcrypt.lib.crypt_rn(key, gensalt, hashed, len(hashed))
+
+print(_bcrypt.ffi.string(hashed))
